82d0b0995f84ed8629b651171a0f7dcba3b5662e
mukul4129 <mukulsharma98260@gmail.com>
2021-08-23 16:54:58 +0530

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root "naukri#index"
-  
-  resources :naukris
-  resources :registrations
-  resources :users
 
+ 
+  resources :naukris 
+  
+  resources :registrations
+ 
+  resources :users
+  get "signup" => "users#new"
   #get "naukri" => "naukri#index"
   
 end
